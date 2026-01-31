cb9f8858a40941a38419f764da5d1e0939ed8ed2
dvrisa <38985578+Riku-S@users.noreply.github.com>
2026-01-31 21:20:43 +0200

    Add script for player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,30 @@ public class Player : MonoBehaviour
     public float speed = 1;
     public float jumpSpeed = 10;
 
+    public Sprite[] IdleSprites;
+    public Sprite[] RunningSprites;
+    public Sprite[] JumpingSprites;
+
+    public float spriteFPS = 6;
+
+    enum PlayerState
+    {
+        Idle, 
+        Running, 
+        Jumping
+    }
+
     private Rigidbody2D rb;
-    private bool isOnGround = true;
     private float xSpeed = 0;
     private bool jumpButtonPressed = false;
     private bool jumpButtonReleased = true;
     private bool jumping = false;
+    private PlayerState state = PlayerState.Idle;
+    private SpriteRenderer sr;
 
     bool IsGrounded()
     {
-        return Physics2D.Raycast(transform.position, Vector2.down, 0.41f);
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.42f);
     }
 
 
@@ -24,6 +38,48 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void HandleSprites()
+    {
+        if (!IsGrounded())
+        {
+            state = PlayerState.Jumping;
+        }
+        else if (Mathf.Abs(xSpeed) > 0.01 || Mathf.Abs(rb.linearVelocityX) > 0.01)
+        {
+            state = PlayerState.Running;
+        }
+        else
+        {
+            state = PlayerState.Idle;
+        }
+
+        if (xSpeed > 0.01)
+        {
+            sr.flipX = false;
+        }
+        else if (xSpeed < -0.01)
+        {
+            sr.flipX = true;
+        }
+
+        Sprite[] sprites = IdleSprites;
+        switch (state)
+        {
+            case PlayerState.Idle:
+                sprites = IdleSprites;
+                break;
+            case PlayerState.Running:
+                sprites = RunningSprites;
+                break;
+            case PlayerState.Jumping:
+                sprites = JumpingSprites;
+                break;
+        }
+        int spriteIndex = Mathf.FloorToInt(Mathf.Repeat(Time.time * spriteFPS, sprites.Length));
+        sr.sprite = sprites[spriteIndex];
     }
 
     // Update is called once per frame
@@ -41,6 +97,8 @@ public class Player : MonoBehaviour
                 Debug.Log("GROUNDED");
             }
         }
+
+        HandleSprites();
     }
 
     void FixedUpdate()
